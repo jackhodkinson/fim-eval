94415d8a2384885be7c82ced5b90ac104d25d5ed
Jack Hodkinson <jackhodkinson93@gmail.com>
2025-01-21 16:58:08 +0000

    Enhance main.py to include functional correctness evaluation and update VSCode settings for Python analysis paths.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from human_eval_infilling.evaluate_functional_correctness import (
+    evaluate_functional_correctness,
+)
 from human_eval_infilling.generate_samples import generate_samples
 
 
 def main():
     generate_samples("test", 1)
+    evaluate_functional_correctness("test", "data/samples.jsonl")
 
 
 if __name__ == "__main__":
